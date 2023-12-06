2b1e076381d336a5557365f475e5fb83b806ae61
Syed Ashif M <syedashif71@gmail.com>
2023-12-06 22:14:45 +0530

    crud application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpTableComponent } from './emp-table/emp-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'emp-table',component:EmpTableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
